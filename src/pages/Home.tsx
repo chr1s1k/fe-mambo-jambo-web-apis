0d23b8087586c4fd86a8c5d3d379bbb167512331
Radek sukup <sukup.r@gmail.com>
2025-02-08 17:27:15 +0100

    Add ScreenWakeLock example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,19 +4,19 @@ export default function Home() {
   return (
     <>
       <h1>Examples</h1>
-      <ol className="list-decimal list-inside">
-        <li className="list__item">
-          <Link to="/screen-wake-lock" className="list__link">
+      <ol className="list-decimal pl-5">
+        <li className="border-b border-solid border-gray-500">
+          <Link to="/screen-wake-lock" className="block px-2 py-3">
             Screen Wake Lock API
           </Link>
         </li>
-        <li className="list__item">
-          <Link to="/page-visibility" className="list__link">
+        <li className="border-b border-solid border-gray-500">
+          <Link to="/page-visibility" className="block px-2 py-3">
             Page Visibility API
           </Link>
         </li>
-        <li className="list__item">
-          <Link to="/battery-status" className="list__link">
+        <li>
+          <Link to="/battery-status" className="block px-2 py-3">
             Battery Status API
           </Link>
         </li>
--- src/pages/ScreenWakeLock.tsx
+++ src/pages/ScreenWakeLock.tsx
@@ -1,3 +1,89 @@
+import { useRef, useState } from "react"
+import clsx from "clsx"
+
 export default function ScreenWakeLock() {
-  return <h1>Screen Wake Lock API</h1>
+  const isSupported = "wakeLock" in navigator
+
+  // create a reference for the Wake Lock sentinel
+  const wakeLock = useRef<WakeLockSentinel | null>(null)
+
+  // create a state for the released status
+  const [released, setReleased] = useState<boolean | undefined>(undefined)
+
+  const requestWakeLock = async () => {
+    if (!isSupported) {
+      console.error("Screen Wake Lock API is not supported in this browser.")
+      return
+    }
+
+    try {
+      wakeLock.current = await navigator.wakeLock.request("screen")
+      setReleased(wakeLock.current.released)
+
+      wakeLock.current.addEventListener("release", () => {
+        setReleased(wakeLock.current?.released)
+      })
+    } catch (err) {
+      console.error(`Wake Lock request failed: ${err}`)
+    }
+  }
+
+  const releaseWakeLock = async () => {
+    wakeLock.current && (await wakeLock.current.release())
+  }
+
+  return (
+    <>
+      <h1>Screen Wake Lock API</h1>
+      <p className="mb-8">
+        {isSupported ? (
+          <>
+            🎉 Hooray! Your browser <strong>supports</strong> Screen Wake Lock API.
+          </>
+        ) : (
+          <>
+            ⛔ Your browser <strong>doesn't support</strong> Screen Wake Lock API.
+          </>
+        )}
+      </p>
+      {isSupported ? (
+        <>
+          <button
+            className={clsx("btn btn-lg", released === false ? "btn-error" : "btn-primary")}
+            onClick={released === false ? releaseWakeLock : requestWakeLock}
+          >
+            Turn Screen Wake Lock {released === false ? "OFF" : "ON"}
+          </button>
+          {/* <div className="mt-8">
+            <label htmlFor="wakeLock" className="flex items-center gap-2">
+              <input
+                id="wakeLock"
+                type="checkbox"
+                checked={released === false}
+                className={clsx("toggle toggle-xl", {
+                  "toggle-success": released === false,
+                })}
+                onChange={released === false ? releaseWakeLock : requestWakeLock}
+              />
+              <span>Prevent my screen from turning off</span>
+            </label>
+          </div> */}
+          <div className="flex items-center mt-8 gap-2">
+            <h2 className="text-xl">Wake Lock Status:</h2>
+            <div
+              className={clsx("status rounded-full size-6", {
+                "status-success": released === false,
+                "status-error": released,
+              })}
+            ></div>
+            <span>
+              {released === undefined && "Not requested"}
+              {released === false && "Active"}
+              {released && "Released"}
+            </span>
+          </div>
+        </>
+      ) : null}
+    </>
+  )
 }
