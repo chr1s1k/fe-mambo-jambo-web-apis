cda2fa78969bc33918ffc59d3a2c5ee476b20886
Radek sukup <sukup.r@gmail.com>
2025-02-13 23:42:44 +0100

    Hide countdown example

--- src/pages/PageVisibility.tsx
+++ src/pages/PageVisibility.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react"
 import { ModalQR } from "../components"
 
 let playingOnHide = false
-const initialCountdownValue = 99
+// const initialCountdownValue = 99
 
 export default function PageVisibility() {
   const [audioAutoPause, setAudioAutoPause] = useState(false)
@@ -32,39 +32,39 @@ export default function PageVisibility() {
 
   // ---------------------------------------------------------
 
-  const [countdownAutoPause, setCountdownAutoPause] = useState(false)
-  const [countdownValue, setCountdownValue] = useState(initialCountdownValue)
+  // const [countdownAutoPause, setCountdownAutoPause] = useState(false)
+  // const [countdownValue, setCountdownValue] = useState(initialCountdownValue)
 
-  useEffect(() => {
-    // countdown
-    const timer = setTimeout(() => {
-      setCountdownValue((val) => {
-        return val <= 0 ? initialCountdownValue : val - 1
-      })
-    }, 1000)
+  // useEffect(() => {
+  //   // countdown
+  //   const timer = setTimeout(() => {
+  //     setCountdownValue((val) => {
+  //       return val <= 0 ? initialCountdownValue : val - 1
+  //     })
+  //   }, 1000)
 
-    const handleVisibilityChange = () => {
-      if (document.hidden) {
-        // pause the countdown
-        clearTimeout(timer)
-      } else {
-        // resume the countdown
-        if (countdownValue > 0) {
-          setCountdownValue(countdownValue - 1)
-        }
-      }
-    }
+  //   const handleVisibilityChange = () => {
+  //     if (document.hidden) {
+  //       // pause the countdown
+  //       clearTimeout(timer)
+  //     } else {
+  //       // resume the countdown
+  //       if (countdownValue > 0) {
+  //         setCountdownValue(countdownValue - 1)
+  //       }
+  //     }
+  //   }
 
-    if (countdownAutoPause) {
-      document.addEventListener("visibilitychange", handleVisibilityChange)
-    }
+  //   if (countdownAutoPause) {
+  //     document.addEventListener("visibilitychange", handleVisibilityChange)
+  //   }
 
-    // cleanup
-    return () => {
-      clearTimeout(timer)
-      document.removeEventListener("visibilitychange", handleVisibilityChange)
-    }
-  }, [countdownValue, countdownAutoPause])
+  //   // cleanup
+  //   return () => {
+  //     clearTimeout(timer)
+  //     document.removeEventListener("visibilitychange", handleVisibilityChange)
+  //   }
+  // }, [countdownValue, countdownAutoPause])
 
   return (
     <>
@@ -75,7 +75,7 @@ export default function PageVisibility() {
           url="https://fe-mambo-jambo-web-apis.vercel.app/page-visibility"
         />
       </div>
-      <section className="min-h-svh">
+      <section>
         <h2 className="text-2xl mb-4">Music player</h2>
         <div className="mb-4">
           <label htmlFor="autoPauseAudio" className="flex items-center gap-2 cursor-pointer">
@@ -94,7 +94,7 @@ export default function PageVisibility() {
         <audio src="/assets/sw-the-imperial-march.mp3" className="w-full" ref={audio} controls />
       </section>
       {/* ----------------------------------------------------------------------------------- */}
-      <section className="min-h-svh">
+      {/* <section className="min-h-svh">
         <h2 className="text-2xl mb-4">Countdown</h2>
         <div className="mb-4">
           <label htmlFor="autoPauseCountdown" className="flex items-center gap-2 cursor-pointer">
@@ -113,7 +113,7 @@ export default function PageVisibility() {
         <span className="countdown font-mono text-6xl">
           <span style={{ "--value": countdownValue } as React.CSSProperties}>{countdownValue}</span>
         </span>
-      </section>
+      </section> */}
     </>
   )
 }
