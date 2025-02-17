e3ed2be0f8a1092b387f7ff9044879ec6213b308
Radek sukup <sukup.r@gmail.com>
2025-02-17 22:45:41 +0100

    Cleanup examples

--- src/pages/PageVisibility.tsx
+++ src/pages/PageVisibility.tsx
@@ -3,7 +3,6 @@ import { useEffect, useRef, useState } from "react"
 import { ModalQR } from "../components"
 
 let playingOnHide = false
-// const initialCountdownValue = 99
 
 export default function PageVisibility() {
   const [audioAutoPause, setAudioAutoPause] = useState(false)
@@ -29,42 +28,6 @@ export default function PageVisibility() {
     // cleanup
     return () => document.removeEventListener("visibilitychange", handleVisibilityChange)
   }, [audioAutoPause])
-
-  // ---------------------------------------------------------
-
-  // const [countdownAutoPause, setCountdownAutoPause] = useState(false)
-  // const [countdownValue, setCountdownValue] = useState(initialCountdownValue)
-
-  // useEffect(() => {
-  //   // countdown
-  //   const timer = setTimeout(() => {
-  //     setCountdownValue((val) => {
-  //       return val <= 0 ? initialCountdownValue : val - 1
-  //     })
-  //   }, 1000)
-
-  //   const handleVisibilityChange = () => {
-  //     if (document.hidden) {
-  //       // pause the countdown
-  //       clearTimeout(timer)
-  //     } else {
-  //       // resume the countdown
-  //       if (countdownValue > 0) {
-  //         setCountdownValue(countdownValue - 1)
-  //       }
-  //     }
-  //   }
-
-  //   if (countdownAutoPause) {
-  //     document.addEventListener("visibilitychange", handleVisibilityChange)
-  //   }
-
-  //   // cleanup
-  //   return () => {
-  //     clearTimeout(timer)
-  //     document.removeEventListener("visibilitychange", handleVisibilityChange)
-  //   }
-  // }, [countdownValue, countdownAutoPause])
 
   return (
     <>
@@ -94,26 +57,6 @@ export default function PageVisibility() {
         <audio src="/assets/sw-the-imperial-march.mp3" className="w-full" ref={audio} controls />
       </section>
       {/* ----------------------------------------------------------------------------------- */}
-      {/* <section className="min-h-svh">
-        <h2 className="text-2xl mb-4">Countdown</h2>
-        <div className="mb-4">
-          <label htmlFor="autoPauseCountdown" className="flex items-center gap-2 cursor-pointer">
-            <input
-              id="autoPauseCountdown"
-              type="checkbox"
-              checked={countdownAutoPause}
-              className={clsx("toggle toggle-xl", {
-                "toggle-success": countdownAutoPause,
-              })}
-              onChange={() => setCountdownAutoPause((prev) => !prev)}
-            />
-            <span>Automatically pause the countdown when the page is hidden</span>
-          </label>
-        </div>
-        <span className="countdown font-mono text-6xl">
-          <span style={{ "--value": countdownValue } as React.CSSProperties}>{countdownValue}</span>
-        </span>
-      </section> */}
     </>
   )
 }
